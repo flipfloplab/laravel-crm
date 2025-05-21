157655315faade2a4d5412510ec3d0b5d7f1fe75
guedespietro5 <pietro.martins@flipfloplab.com.br>
2025-05-21 14:07:15 -0300

    Multi-675: Criação e pré configuração de arquivos dependentes para o Multi Tenant

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -15,21 +15,35 @@ if echo "$INSTALLED" | grep -q "core_config"; then
   echo "Krayin já está instalado. Pulando instalação..."
 else
   echo "Krayin ainda não instalado. Executando instalação..."
+  echo "======================================"
+
   composer install
 
   composer create-project ...
 
   php artisan krayin-crm:install
 
+  #CONFIGURAÇÕES API 
+  echo "Iniciando configuração da API"
+  echo "======================================"
   echo "nameserver 8.8.8.8" > /etc/resolv.conf
   echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 
   composer config -g repo.packagist composer https://mirrors.cloud.tencent.com/composer/
 
   composer clear-cache
+
   composer require krayin/rest-api -vvv
 
   php artisan krayin-rest-api:install 
+
+  #CONFIGURAÇÕES MULTI TENANT
+  echo "Iniciando configuração do Multi Tenant"
+  echo "======================================"
+  
+  composer require stancl/tenancy
+  php artisan db:seed
+
 fi
 
 php artisan serve --host=0.0.0.0 --port=8000
